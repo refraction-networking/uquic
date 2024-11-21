e03343975d6f4edec0f44fa376b2c9a52d331133
Mingye Chen <mingye.chen@colorado.edu>
2024-11-21 15:34:28 -0700

    Add example

---
+++ example/chat/listen/main.go
@@ -0,0 +1,113 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"math/big"
+	"net"
+	"time"
+
+	"github.com/pion/dtls/v3/examples/util"
+	quic "github.com/refraction-networking/uquic"
+	tls "github.com/refraction-networking/utls"
+)
+
+const (
+	receiveMTU      = 8192
+	cidSize         = 8
+	keySize         = 32
+	station_privkey = "203963feed62ddda89b98857940f09866ae840f42e8c90160e411a0029b87e60"
+)
+
+type streamConn struct {
+	quic.Stream
+	quic.Connection
+}
+
+func main() {
+	var listenAddr = flag.String("laddr", "0.0.0.0:6666", "listen address")
+
+	flag.Parse()
+
+	// Prepare the IP to connect to
+	addr, err := net.ResolveUDPAddr("udp", *listenAddr)
+	util.Check(err)
+
+	priv, err := hex.DecodeString(station_privkey)
+	util.Check(err)
+
+	fmt.Printf("%v\n", priv)
+
+	pconn, err := net.ListenUDP("udp", addr)
+	util.Check(err)
+	tp := quic.Transport{
+		Conn: pconn,
+	}
+
+	listener, err := tp.ListenEarly(generateTLSConfig(), &quic.Config{})
+	util.Check(err)
+
+	// Simulate a chat session
+	hub := util.NewHub()
+
+	go func() {
+		for {
+			// Wait for a connection.
+			econn, err := listener.Accept(context.Background())
+			if err != nil {
+				continue
+			}
+
+			for i := 0; i < 2; i++ {
+				ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+				stream, err := econn.AcceptStream(ctx)
+				if err != nil {
+					continue
+				}
+				hub.Register(&streamConn{Stream: stream, Connection: econn})
+
+			}
+
+			// `conn` is of type `net.Conn` but may be casted to `dtls.Conn`
+			// using `dtlsConn := conn.(*dtls.Conn)` in order to to expose
+			// functions like `ConnectionState` etc.
+
+			// Register the connection with the chat hub
+		}
+	}()
+
+	// Start chatting
+	hub.Chat()
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"h3"},
+	}
+}
--- go.mod
+++ go.mod
@@ -6,15 +6,16 @@ require (
 	github.com/francoispqt/gojay v1.2.13
 	github.com/onsi/ginkgo/v2 v2.17.2
 	github.com/onsi/gomega v1.33.1
+	github.com/pion/dtls/v3 v3.0.4
 	github.com/quic-go/qpack v0.4.0
 	github.com/refraction-networking/clienthellod v0.5.0-alpha2
 	github.com/refraction-networking/utls v1.6.6
 	go.uber.org/mock v0.4.0
-	golang.org/x/crypto v0.23.0
+	golang.org/x/crypto v0.28.0
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
-	golang.org/x/net v0.25.0
-	golang.org/x/sync v0.7.0
-	golang.org/x/sys v0.20.0
+	golang.org/x/net v0.30.0
+	golang.org/x/sync v0.8.0
+	golang.org/x/sys v0.26.0
 	golang.org/x/time v0.5.0
 )
 
@@ -27,9 +28,10 @@ require (
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/pprof v0.0.0-20240430035430-e4905b036c4e // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	golang.org/x/tools v0.21.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
