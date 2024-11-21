22276ac4648ab756412e16ae411ac4478339e3e6
Mingye Chen <mingye.chen@colorado.edu>
2024-11-21 15:34:28 -0700

    Add example

---
+++ example/chat/dial/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+
+	"github.com/pion/dtls/v3/examples/util"
+	quic "github.com/refraction-networking/uquic"
+	tls "github.com/refraction-networking/utls"
+)
+
+func main() {
+	var remoteAddr = flag.String("raddr", "127.0.0.1:6666", "remote address")
+	// var pubkey = flag.String("secret", "0b63baad7f2f4bb5b547c53adc0fbb179852910607935e6f4b5639fd989b1156", "shared secret")
+	// var covert = flag.String("covert", "1.2.3.4:5678", "covert address")
+	flag.Parse()
+
+	addr, err := net.ResolveUDPAddr("udp", *remoteAddr)
+	util.Check(err)
+
+	// pubkeyBytes, err := hex.DecodeString(*pubkey)
+	// util.Check(err)
+
+	pconn, err := net.ListenUDP("udp", nil)
+	util.Check(err)
+	quicSpec, err := quic.QUICID2Spec(quic.QUICFirefox_116)
+	util.Check(err)
+	for _, ext := range quicSpec.ClientHelloSpec.Extensions {
+		if ks, ok := ext.(*tls.KeyShareExtension); ok {
+			ks.KeyShares = []tls.KeyShare{
+				{
+					Group: tls.X25519Kyber768Draft00,
+					Data:  []byte{},
+				},
+			}
+			break
+		}
+	}
+
+	tp := quic.UTransport{
+		Transport: &quic.Transport{
+			Conn: pconn,
+		},
+		QUICSpec: &quicSpec,
+	}
+
+	// tp := &quic.Transport{
+	// 	Conn: pconn,
+	// }
+
+	// econn1, err := tp.DialEarly(context.Background(), addr, &tls.Config{
+	// 	InsecureSkipVerify: true,
+	// 	NextProtos:         []string{"h3"},
+	// }, &quic.Config{})
+	// util.Check(err)
+	// _ = econn1
+
+	econn, err := tp.DialEarly(context.Background(), addr, &tls.Config{
+		InsecureSkipVerify: true,
+		// CurvePreferences:   []tls.CurveID{tls.X25519Kyber768Draft00},
+		NextProtos: []string{"h3"},
+	}, &quic.Config{})
+	util.Check(err)
+
+	stream, err := econn.OpenStream()
+	util.Check(err)
+
+	stream2, err := econn.OpenStream()
+	util.Check(err)
+
+	fmt.Println("Connected; type 'exit' to shutdown gracefully")
+
+	stream2.Write([]byte("testt\n"))
+
+	// Simulate a chat session
+	util.Chat(stream)
+
+}
--- go.mod
+++ go.mod
@@ -6,16 +6,17 @@ require (
 	github.com/francoispqt/gojay v1.2.13
 	github.com/onsi/ginkgo/v2 v2.17.2
 	github.com/onsi/gomega v1.33.1
+	github.com/pion/dtls/v3 v3.0.4
 	github.com/quic-go/qpack v0.4.0
 	github.com/refraction-networking/clienthellod v0.5.0-alpha2
 	github.com/refraction-networking/utls v1.6.7
 	go.uber.org/mock v0.4.0
-	golang.org/x/crypto v0.31.0
-	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
-	golang.org/x/net v0.32.0
-	golang.org/x/sync v0.10.0
-	golang.org/x/sys v0.28.0
-	golang.org/x/time v0.8.0
+	golang.org/x/crypto v0.28.0
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
+	golang.org/x/net v0.30.0
+	golang.org/x/sync v0.8.0
+	golang.org/x/sys v0.26.0
+	golang.org/x/time v0.5.0
 )
 
 require (
@@ -27,8 +28,10 @@ require (
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
-	golang.org/x/mod v0.19.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/tools v0.23.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
